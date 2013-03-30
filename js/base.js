9a0db733c4e286d8703a91d9fbaca20d0d2f3866
jeancamargo <jeanmmgc@gmail.com>
2013-03-30 06:48:07 -0300

    Java modelado

--- js/base.js
+++ js/base.js
@@ -1,39 +1,79 @@
-$(document).ready(function() {
-/*
-Criando botões e o rolê de navegação para o carrocel
-		<div id="prev">Prev</h3>
-		<h3 id="next">Next</h3>
-		<div id="cacete"></div>
-*/
-$("#exposicao").hide();
-$("#editorial").hide();
-$("#expediente").hide();
-
-//$('<br />').insertAfter('.titulo');
-
-$("#trabalhos").append("<div id='prev'><img src='img/estilo/prev_button.png'></div><div id='next'><img src='img/estilo/next_button.png'>")
+function mudapagina(z){
+	clearPage();
+	if (z==""){
+		z = "#exposicao";
+	}
+	$(z).fadeIn();
+}
+function clearPage(){
+	$("#exposicao").hide();
+	$("#editorial").hide();
+	$("#expediente").hide();
+	$("#studium").hide();
+}
+function carroceu(z){
+	/*Create navigation buttons to use with CarrouFredSel*/
+	$(z).append("<div id='prev'><img src='img/estilo/prev_button.png'></div><div id='next'><img src='img/estilo/next_button.png'>")
 	/*	CarouFredSel: a circular, responsive jQuery carousel.
 	Configuration created by the "Configuration Robot"
 	at caroufredsel.dev7studios.com
+	*/
+	z = z+" ul";
+	$(z).carouFredSel({
+		width: "100%",
+		height: 400,
+		items: "variable",
+		scroll: {
+			items: 1,
+			pauseOnHover: true
+		},
+		auto: 7000,
+		prev: {
+			button: "#prev",
+			key: "left"
+		},
+		next: {
+			button: "#next",
+			key: "right"
+		},
+		swipe: true,
+		mousewheel: true
+	});
+}
+
+/*
+###########################################
+Esquema pra toda vez que mudar a pagina atualizar o rolê
+###########################################
 */
-$("#trabalhos ul").carouFredSel({
-	width: "100%",
-	height: 400,
-	items: "variable",
-	scroll: {
-		items: 1,
-		pauseOnHover: true
-	},
-	auto: 7000,
-	prev: {
-		button: "#prev",
-		key: "left"
-	},
-	next: {
-		button: "#next",
-		key: "right"
-	},
-	swipe: true,
-	mousewheel: true
+
+$(window).bind('hashchange', function() {
+	clearPage();
+    _href = window.location.hash;
+	mudapagina(_href);
+	carroceu("#trabalhos");
+	return false
 });
+
+$(document).ready(function() {
+clearPage();
+var _href = window.location.hash;
+alert(_href);
+mudapagina(_href);
+carroceu("#trabalhos");
+if (Modernizr.history) {
+    // history is supported; do magical things
+    // hijack the nav click event
+    $("nav").delegate("a", "click", function() {
+		_href = $(this).attr("href");
+		// change the url without a page refresh and add a history entry.
+		history.pushState(null, null, _href);
+		// load the content
+		mudapagina(_href);
+		// fear not! we're going to build this function in the next code block
+	return false
+	});
+} else {
+
+}
 });
